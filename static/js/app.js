519f784b1dfe3a5ee76d9a51b4ad8229017b5ad7
JMazick <jazlynmm@outlook.com>
2021-01-19 14:03:56 -0600

    add table to html

--- static/js/app.js
+++ static/js/app.js
@@ -1,4 +1,27 @@
 // from data.js
 var tableData = data;
 
+// select tbody from html
+var tbody = d3.select("tbody");
+tbody.html("");
+// iterate info from data
+console.log(tableData)
+
+// Loop through array of objects then each object
+tableData.forEach((ufo) => {
+
+// add a row for each object inside tbody
+var trow = tbody.append("tr");
+
+ // Get the entries for each object in the array
+ Object.entries(ufo).forEach(([key, value]) => {
+    // Log the key and value
+    var tdata = trow.append("td");
+    tdata.text(value);
+  });
+});
+
+
+  
+
 // YOUR CODE HERE!
